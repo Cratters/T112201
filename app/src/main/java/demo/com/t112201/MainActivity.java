c6e84d6d063b160a98a0c1397471c558823cf019
7R <4a2k0055@stust.edu.tw>
2017-12-25 15:48:57 +0800

    Initial commit

---
+++ app/src/main/java/demo/com/t112201/MainActivity.java
@@ -0,0 +1,51 @@
+package demo.com.t112201;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    MyAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+
+    }
+
+    class MyAdapter extends BaseAdapter {
+
+        private LayoutInflater myinflater;
+
+        //public MyAdapter
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return i;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            return view;
+        }
+    }
+}
